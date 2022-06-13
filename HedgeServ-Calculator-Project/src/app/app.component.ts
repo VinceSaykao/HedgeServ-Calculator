0a575088a4d5fce2b039e1cc3753ed5d52b55edd
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-06-13 13:54:57 -0500

    added component files

--- HedgeServ-Calculator-Project/src/app/app.component.ts
+++ HedgeServ-Calculator-Project/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HedgeServ-Calculator-Project';
+  title: string = 'HedgeServ-Calculator-Project';
 }
