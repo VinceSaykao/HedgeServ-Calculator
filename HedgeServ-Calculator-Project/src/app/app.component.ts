7b94d7e17ed1d8d180e1d3cd1b76ee55f05aff82
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-06-14 15:49:02 -0500

    commented out calc value and added comments in app component

--- HedgeServ-Calculator-Project/src/app/app.component.ts
+++ HedgeServ-Calculator-Project/src/app/app.component.ts
@@ -13,27 +13,32 @@ export class AppComponent {
   toshow = '0'
   currvalue = ''
 
+  // attribute binding: 
+
+  // this takes in a pressed number and input it
   writetoinput(value:string){
     this.currvalue = this.currvalue + value
     this.toshow = this.currvalue
-
   }
 
+  // when equal is pressed, it will do calculation and display it
   equals() {
     this.toshow = eval(this.currvalue)
     this.currvalue = this.toshow
   }
 
+  // when clear is pressed, it will clear the display and all inputs to an empty string
   clear() {
     this.currvalue = '';
     this.toshow = this.currvalue
   }
 
-  calcvalue(solve:any) {
-    if(solve.charAt(0) == '0') {
-      solve = solve.slice(1,)
-    }
-    this.toshow = eval(solve)
-  }
+
+  // calcvalue(solve:any) {
+  //   if(solve.charAt(0) == '0') {
+  //     solve = solve.slice(1,)
+  //   }
+  //   this.toshow = eval(solve)
+  // }
 
 }
